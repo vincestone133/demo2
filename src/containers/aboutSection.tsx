cca8ac54c9af91e3597defa4ee1e306a1eb49e9e
nguyễn quang sơn <70741367+vincestone133@users.noreply.github.com>
2023-07-31 17:12:08 +0700

    4

--- src/containers/aboutSection.tsx
+++ src/containers/aboutSection.tsx
@@ -32,7 +32,7 @@ export const AboutQuote = () => {
         $position="static"
         className="img-1"
       ></CircleImg>
-      <h1 className=" svn-text">About Me</h1>
+      <h1 className="svn-text">About Me</h1>
       <p className="text-justify font-bold italic text-[20px]">
         Imagination is everything. It is the preview of life's coming
         attractions.
